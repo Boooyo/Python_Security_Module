2b9b93092e1df9448c99d0eeb1cc221858ef13fa
Boo_oyo <136545068+Boooyo@users.noreply.github.com>
2024-07-26 13:11:53 +0900

    OWASP-ZAP Commit

--- OWASP-ZAP/OWASP-ZAP.py
+++ OWASP-ZAP/OWASP-ZAP.py
@@ -2,39 +2,47 @@ import time
 from pprint import pprint
 from zapv2 import ZAPv2
 
-target = 'http://localhost:8080' #target url for scan
+def wait_for_completion(status_func, status_name):
+    """Waits for a ZAP scan to complete, given a status function."""
+    print(f'{status_name} in progress...')
+    while int(status_func()) < 100:
+        print(f'{status_name} progress %: {status_func()}')
+        time.sleep(5)
+    print(f'{status_name} completed')
 
-zap = ZAPv2(proxies={'http':'http://localhost:8090','https':'http://localhost:8090'})  
+def save_report(report_func, report_name):
+    """Saves a ZAP report to a file."""
+    with open(report_name, 'w') as f:
+        f.write(report_func())
 
-apikey = 'APIKEY'
-print 'Accessing target %s' % target
-zap.urlopen(target)
+def main():
+    target = 'http://localhost:8080'  # target URL for the scan
+    zap = ZAPv2(proxies={'http': 'http://localhost:8090', 'https': 'http://localhost:8090'})
+    apikey = 'APIKEY'
 
-print 'Traditional Spidering target %s' % target
-zap.spider.scan(target)
-time.sleep(5)
-while (int(zap.spider.status()) < 100):
+    print(f'Accessing target {target}')
+    zap.urlopen(target)
+
+    print(f'Traditional Spidering target {target}')
+    zap.spider.scan(target)
     time.sleep(5)
-    print ('Spider progress %: ' + zap.spider.status())
-    time.sleep(5)
-print ('Spider completed')
+    wait_for_completion(zap.spider.status, 'Spider')
 
-print 'Scanning target %s' % target
-zap.ascan.scan(target)
-time.sleep(5)
-while (int(zap.ascan.status()) < 100):
+    print(f'Scanning target {target}')
+    zap.ascan.scan(target)
     time.sleep(5)
-    print ('Ascan progress %: ' + zap.ascan.status())
-    time.sleep(5)
-print ('Ascan completed')
+    wait_for_completion(zap.ascan.status, 'Active Scan')
 
-#Report the results
-print 'Hosts: ' + ', '.join(zap.core.hosts)
-print 'Alerts: '
-pprint (zap.core.alerts()) #prints all alerts. can be commented
-# HTML Report
-with open ('report.html', 'w') as f:f.write(zap.core.htmlreport(apikey = apikey))
-# XML Report
-with open ('report.xml', 'w') as f:f.write(zap.core.xmlreport(apikey = apikey))
+    # Report the results
+    print('Hosts: ' + ', '.join(zap.core.hosts))
+    print('Alerts: ')
+    pprint(zap.core.alerts())  # prints all alerts. can be commented
 
-zap.core.shutdown()
+    # Save HTML and XML reports
+    save_report(lambda: zap.core.htmlreport(apikey=apikey), 'report.html')
+    save_report(lambda: zap.core.xmlreport(apikey=apikey), 'report.xml')
+
+    zap.core.shutdown()
+
+if __name__ == "__main__":
+    main()
