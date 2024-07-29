14eac5af4f5ce633152fe9c6d456c7aeb65d4b93
Boo_oyo <136545068+Boooyo@users.noreply.github.com>
2024-07-29 09:53:19 +0900

    Ha3Mrx.py Commit

--- Ha3Mrx/Ha3Mrx.py
+++ Ha3Mrx/Ha3Mrx.py
@@ -1,45 +1,71 @@
-import os, sys, time
-from time import sleep as timeout
+import os
+import sys
+import time
+
 def restart_program():
-        python = sys.executable
-        os.execl(python, python, * sys.argv)
-        curdir = os.getcwd()
-os.system("clear")
-os.system("figlet Ha3MrX")
-print "Create By : Ha3MrX"
-print "           [1]> Brute Force              "
-print "           [2]> DDos Attack              "
-print "           [3]> NMap PortScanner         "
-print "           [4]> Install Tools Hacking    "
-print 
-print " [0]> Exit "
-print
-A = raw_input("Ha3MrX ==>> ")
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
 
-if A == "1" or A == "01":
-  os.system("python2 brute.py")
-
-elif A == "2" or A == "02":
+def clear_screen():
     os.system("clear")
+
+def display_banner():
+    os.system("figlet Ha3MrX")
+
+def main_menu():
+    clear_screen()
+    display_banner()
+    print("Create By : Ha3MrX")
+    print("           [1]> Brute Force              ")
+    print("           [2]> DDos Attack              ")
+    print("           [3]> NMap PortScanner         ")
+    print("           [4]> Install Tools Hacking    ")
+    print()
+    print(" [0]> Exit ")
+    print()
+
+def get_user_choice():
+    return input("Ha3MrX ==>> ")
+
+def brute_force():
+    os.system("python2 brute.py")
+
+def ddos_attack():
+    clear_screen()
     os.system("figlet DDOS Attack")
-    ip = raw_input("IP Address : ")
-    port = raw_input("Port       : ")
-    packet =raw_input("Packet     : ")
-    os.system("python2 pntddos %s %s %s" % (ip, port, packet))
+    ip = input("IP Address : ")
+    port = input("Port       : ")
+    packet = input("Packet     : ")
+    os.system(f"python2 pntddos {ip} {port} {packet}")
 
-elif A == "3" or A == "03":
-    os.system("clear")
+def nmap_scan():
+    clear_screen()
     os.system("figlet NMap Scan")
-    host = raw_input("Host : ")
-    os.system("nmap %s" % (host))
+    host = input("Host : ")
+    os.system(f"nmap {host}")
 
-elif A == "4" or A == "04":
+def install_tools_hacking():
     os.system("python2 lazymux.py")
-    
-elif A == "0" or A == "00":
-    sys.exit()
-    
-else:
-     print "\nERROR: Wrong Input"
-     timeout(3)
-     restart_program()
+
+def main():
+    while True:
+        main_menu()
+        choice = get_user_choice()
+
+        if choice in ["1", "01"]:
+            brute_force()
+        elif choice in ["2", "02"]:
+            ddos_attack()
+        elif choice in ["3", "03"]:
+            nmap_scan()
+        elif choice in ["4", "04"]:
+            install_tools_hacking()
+        elif choice in ["0", "00"]:
+            sys.exit()
+        else:
+            print("\nERROR: Wrong Input")
+            time.sleep(3)
+            restart_program()
+
+if __name__ == "__main__":
+    main()
